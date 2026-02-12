b0b224c4457b17631db39b196e9e625bc157da35
stoledcat <jetnavo@yandex.ru>
2026-02-12 16:33:34 +0300

    Заглушки для houses

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -4,10 +4,10 @@ houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 @houses_router.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def houses():
+    return {"message": "Дома"}
 
 
-@houses_router.get("/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@houses_router.get("/{house_id}")
+async def house_detail(house_id: int):
+    return {"message": f"Дом {house_id}"}
