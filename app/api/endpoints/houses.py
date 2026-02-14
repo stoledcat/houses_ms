eee71670eedb2afa8c5dfd774c687b5c01ecd2ba
stoledcat <jetnavo@yandex.ru>
2026-02-14 14:33:34 +0300

    Базовая обработка данных

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+from app.data import houses_list
 
 houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 @houses_router.get("/")
 async def houses():
-    return {"message": "Дома"}
+    return houses_list
 
 
 @houses_router.get("/{house_id}")
 async def house_detail(house_id: int):
-    return {"message": f"Дом {house_id}"}
+    for house in houses_list:
+        if house["id"] == house_id:
+            return house
+    raise HTTPException(status_code=404, detail="House not found")
