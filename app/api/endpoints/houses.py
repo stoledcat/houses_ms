c17a2958b731cdb922228c83732f7bf103392452
stoledcat <jetnavo@yandex.ru>
2026-02-14 17:36:10 +0300

    Добавил первый фильтр данных

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -2,19 +2,30 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from app.data import houses_list
 from app.schemas.house import HouseDetailSchema, HouseItemSchema
+from fastapi import Query
 
-from typing import List
+from typing import List, Optional
 
 houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
 @houses_router.get("/", response_model=List[HouseItemSchema])
-async def houses():
-    return houses_list
+async def get_houses(
+    min_price: Optional[int] = Query(None, ge=0), max_price: Optional[int] = Query(None, ge=0)
+):
+    houses = [h for h in houses_list if h["active"]]
+
+    if min_price is not None:
+        houses = [h for h in houses if h["price"] >= min_price]
+
+    if max_price is not None:
+        houses = [h for h in houses if h["price"] <= max_price]
+
+    return houses
 
 
 @houses_router.get("/{house_id}", response_model=HouseDetailSchema)
-async def house_detail(house_id: int):
+async def get_house_detail(house_id: int):
     for house in houses_list:
         if house["id"] == house_id:
             return house
