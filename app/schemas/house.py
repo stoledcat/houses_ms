8aa29b080988aaf04f12e9f67783178f22eb868b
stoledcat <jetnavo@yandex.ru>
2026-02-18 16:02:59 +0300

    Добавил описание к схемам

--- app/schemas/house.py
+++ app/schemas/house.py
@@ -2,22 +2,22 @@ from pydantic import BaseModel, Field
 
 
 class HouseFullSchema(BaseModel):
-    id: int = Field(description="ID объявления")
-    name: str = Field(description="Название")
-    description: str = Field(description="Описание")
-    price: int = Field(description="Стоимость")
-    active: bool = Field(description="Объявление активно")
+    id: int
+    name: str = Field(description="Название дома")
+    description: str = Field(description="Краткое описание")
+    price: int = Field(description="Цена в рублях", examples=["5000", "10000"])
+    active: bool
 
 
 
 class HouseDetailSchema(BaseModel):
-    id: int = Field(description="ID объявления")
-    name: str = Field(description="Название")
-    description: str = Field(description="Описание")
-    price: int = Field(description="Стоимость")
+    id: int
+    name: str = Field(description="Название дома")
+    description: str = Field(description="Краткое описание")
+    price: int = Field(description="Цена в рублях", examples=["5000", "10000"])
 
 
 class HouseItemSchema(BaseModel):
     id: int = Field(description="ID объявления")
-    name: str = Field(description="Название")
-    price: int = Field(description="Стоимость")
+    name: str = Field(description="Название дома")
+    price: int = Field(description="Цена в рублях", examples=["5000", "10000"])
