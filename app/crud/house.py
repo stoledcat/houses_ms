9e828ae5af9e23f2dcb2b3c91338caecc6bd6833
stoledcat <jetnavo@yandex.ru>
2026-02-23 22:43:01 +0300

    Поиск по названию и описанию дома

--- app/crud/house.py
+++ app/crud/house.py
@@ -1,15 +1,20 @@
-from sqlmodel import Session, asc, desc, select
+from sqlmodel import Session, asc, desc, select, or_
+from typing import Sequence
 
 from app.models import House
 
 
-def get_active_houses(session: Session):
+def get_active_houses(session: Session) -> Sequence[House]:
     stmt = select(House).where(House.active)
     return session.exec(stmt).all()
 
 
-def get_filtered_active_houses(session: Session, min_price=None, max_price=None, order_by="id", order="asc"):
+def get_filtered_active_houses(session: Session, search=None, min_price=None, max_price=None, order_by="id", order="asc"):
     stmt = select(House).where(House.active)
+
+    if search:
+        # stmt = stmt.where(House.description.icontains(search) | House.name.icontains(search))
+        stmt = stmt.where(or_(House.description.icontains(search), House.name.icontains(search)))
 
     if min_price:
         stmt = stmt.where(House.price >= min_price)
