94f5aeed9bfde9d13afee6170168e9147049a074
stoledcat <jetnavo@yandex.ru>
2026-03-02 21:10:52 +0300

    Закрытие сессий

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api.endpoints.houses import houses_router
+from app.db.session import async_engine
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Начало")
+
     yield
-    print("Завершение")
+    async_engine.dispose()
 
 
 app = FastAPI(lifespan=lifespan, title="API домов", description="Микросервис для управления домами")
