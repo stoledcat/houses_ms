8fc7cfc182b326f81f49878fe6612fd05638404a
stoledcat <jetnavo@yandex.ru>
2026-03-03 16:20:47 +0300

    Расширил модель дома

--- app/models/house.py
+++ app/models/house.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, SQLModel
+import sqlalchemy as sa
 
 
 class House(SQLModel, table=True):
@@ -9,3 +10,8 @@ class House(SQLModel, table=True):
     description: str = Field(default="")
     price: int = Field(default=0, index=True)
     active: bool = Field(default=True, index=True)
+
+    square: int | None = Field(default=None)
+    rooms: int | None = Field(default=1)
+    bathrooms: int = Field(default=1, nullable=True)
+    free_parking: bool = Field(default=False, sa_column=sa.Column(sa.Boolean, server_default=sa.text("FALSE"), nullable=False))
